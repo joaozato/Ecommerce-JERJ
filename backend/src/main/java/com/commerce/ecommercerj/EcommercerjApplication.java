c77bc04d2944110fb8641e6a236ce4ddb6baa3e0
João <rodriguesjoovtor@gmail.com>
2026-05-12 13:56:26 -0300

    1

---
+++ backend/src/main/java/com/commerce/ecommercerj/EcommercerjApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.ecommercerj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercerjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercerjApplication.class, args);
+	}
+
+}
