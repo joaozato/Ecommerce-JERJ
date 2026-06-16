1134feb5f7bd38017e5d431585398e13d34d2599
JHOWSEF <jose.filipe.gomes@aluno.riogrande.ifrs.edu.br>
2026-06-15 23:13:33 -0300

    fix readme

--- backend/src/main/java/com/commerce/ecommercerj/infrastructure/entitys/produtos.java
+++ backend/src/main/java/com/commerce/ecommercerj/infrastructure/entitys/produtos.java
@@ -14,7 +14,7 @@ import lombok.*;
 
 public class produtos {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @Column(name = "Nome", nullable = false)
